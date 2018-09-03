6698ae4bbb52749075a0264c92e9523eab6c8d9b
DAVID HO <davidhorph@hotmail.com>
2018-09-03 02:57:37 -0500

    Added the tables to Cities and Countries

--- VacationBiddingSite/Data/Migrations/20180903075349_CitiesCountriesHotels.Designer.cs
+++ VacationBiddingSite/Data/Migrations/20180903075349_CitiesCountriesHotels.Designer.cs
@@ -11,9 +11,10 @@ using VacationBiddingSite.Data;
 namespace VacationBiddingSite.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180903075349_CitiesCountriesHotels")]
+    partial class CitiesCountriesHotels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ VacationBiddingSite/Data/Migrations/20180903075349_CitiesCountriesHotels.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace VacationBiddingSite.Data.Migrations
+{
+    public partial class CitiesCountriesHotels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "city",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_city", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "country",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_country", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "hotel",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    AverageRoomRate = table.Column<double>(nullable: false),
+                    Restaurant24Hour = table.Column<bool>(nullable: false),
+                    buffet = table.Column<bool>(nullable: false),
+                    name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_hotel", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "city");
+
+            migrationBuilder.DropTable(
+                name: "country");
+
+            migrationBuilder.DropTable(
+                name: "hotel");
+        }
+    }
+}
